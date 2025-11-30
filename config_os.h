d45e8b33b8b22f5c2a02bac07ecf3764e9455a50
Gocnak <gocnak@momentum-mod.org>
2025-11-30 15:43:22 -0800

    Supported configuration, actually

--- config_os.h
+++ config_os.h
@@ -29,9 +29,9 @@
 // https://www.cryptopp.com/wiki/Release_Process#Self_Tests
 // The problems with Clang pretending to be other compilers is
 // discussed at http://github.com/weidai11/cryptopp/issues/147.
-#if (defined(_MSC_VER) && defined(__clang__))
-# error: "Unsupported configuration"
-#endif
+//#if (defined(_MSC_VER) && defined(__clang__))
+//# error: "Unsupported configuration"
+//#endif
 
 // Windows platform
 #if defined(_WIN32) || defined(_WIN64) || defined(__CYGWIN__)
